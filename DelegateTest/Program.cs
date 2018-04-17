1fbbeba2a2d6878f7b5171a68628c76437a104a2
f472918106 <472918106@qq.com>
2018-04-17 18:36:10 +0800

    规范委托写法

--- DelegateTest/Program.cs
+++ DelegateTest/Program.cs
@@ -6,58 +6,91 @@ using System.Threading.Tasks;
 
 namespace DelegateTest
 {
-    // Observer设计模式
+    // 热水器
     public class Heater
     {
-        private int temperature;//水温 
-        public delegate void BoilHandler(int param); //声明委托
-        public event BoilHandler BoilEvent; //声明事件
+        private int temperature;
+        public string type = "RealFire 001"; //添加型号作为演示 
+        public string area = "China Xian"; //添加产地作为演示
+        //声明委托
+        public delegate void BoiledEventHandler(Object sender,BoiledEventArgs e);
+        public event BoiledEventHandler Boiled; //声明事件
+
+        //定义BoiledEventArgs类，传递给Observer所感兴趣的信息
+        public class BoiledEventArgs : EventArgs
+        {
+            public readonly int temperature;
+            public BoiledEventArgs(int temperature)
+            {
+                this.temperature = temperature;
+            }
+        }
+
+        //可以供继承自Heater的类重写，以便继承类拒绝其他对象对它的监视
+        protected virtual void OnBoiled(BoiledEventArgs e)
+        {
+            if(Boiled != null) 
+            {
+                Boiled(this, e); //如果有对象注册，调用所有注册对象的方法
+            }
+        }
+
+        //烧水
         public void BoilWater()
         {
-            for (int i = 0; i <= 100; i++)
+            for(int i = 0; i <= 100; i++)
             {
                 temperature = i;
                 if (temperature > 95)
                 {
-                    if (BoilEvent != null) //如果有对象注册
-                    {
-                        BoilEvent(temperature); //调用所注册对象的方法
-                    }
+                    //建立BoiledEventArgs对象
+                    BoiledEventArgs e = new BoiledEventArgs(temperature);
+                    OnBoiled(e); //调用OnBoiled方法
                 }
-            } 
-        }
-
-        //警报器
-        public class Alarm
-        {
-            public void MakeAlert(int param)
-            {
-                Console.WriteLine("Alarm: 滴滴滴，水已经{0}度了。", param);
             }
         }
+    }
 
-        //显示器
-        public class Display
+    //警报器
+    public class Alarm
+    {
+        public void MakeAlert(Object sender, Heater.BoiledEventArgs e)
         {
-            public static void ShowMsg(int param) //静态方法
-            {
-                Console.WriteLine("Display: 水快开了，当前温度:{0}度。", param);
-            }
+            Heater heater = (Heater)sender;
+            //访问sender中的公共字段
+            Console.WriteLine("Alarm:{0}-{1}:", heater.area, heater.type);
+            Console.WriteLine("Alarm: 滴滴滴,水已经{0}度了;", e.temperature);
+            Console.WriteLine();
         }
+    }
 
-        class Program
+    //显示器
+    public class Display
+    {
+        public static void ShowMsg(Object sender, Heater.BoiledEventArgs e)
         {
-            static void Main()
-            {
-                Heater heater = new Heater();
-                Alarm alarm = new Alarm();
+            //静态方法
+            Heater heater = (Heater)sender;
+            Console.WriteLine("Display:{0}-{1};", heater.area, heater.type);
+            Console.WriteLine("Display: 水快烧开了,当前温度:{0}度。", e.temperature);
+            Console.WriteLine();
+        }
+    }
 
-                heater.BoilEvent += alarm.MakeAlert; //注册方法
-                heater.BoilEvent += (new Alarm()).MakeAlert; //给匿名对象注册方法
-                heater.BoilEvent += Display.ShowMsg; //注册静态方法
-                heater.BoilWater(); //烧水，会自动调用注册过对象的方法
-                Console.ReadKey();
-            }
+    class Program
+    {
+        static void Main()
+        {
+            Heater heater = new Heater();
+            Alarm alarm = new Alarm();
+
+            heater.Boiled += alarm.MakeAlert; //注册方法
+            heater.Boiled += (new Alarm()).MakeAlert; //给匿名对象注册方法
+            heater.Boiled += new Heater.BoiledEventHandler(alarm.MakeAlert); //也可以这么注册
+            heater.Boiled += Display.ShowMsg; //注册静态方法
+
+            heater.BoilWater(); //烧水，会自动调用注册过对象的方法
+            Console.ReadKey();
         }
     }
 }
